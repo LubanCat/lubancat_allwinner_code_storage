91bb3d4614e92916d7f8bc3c642f621181ee48f7
qianzhongyang <3295423372@qq.com>
2024-02-28 08:59:44 +0000

    提交Linux驱动helloworld例程

---
+++ linux_driver/README.md
@@ -0,0 +1,24 @@
+# 鲁班猫全志系列驱动教程配套例程
+
+## 使用
+
+把该例程linux_driver放到内核源码的同级目录下：
+
+.
+
+├── kernel
+
+└── linux_driver
+
+linux_driver中内核模块makefile的KERNEL_DIR都是指向kernel/build。
+
+## 环境
+
+服务器/PC系统：ubuntu20.04
+
+板卡系统：ubuntu22.04
+
+## 配套教程
+
+**《 嵌入式Linux驱动开发实战指南——基于鲁班猫全志系列板卡 》**
+
---
+++ linux_driver/helloworld/helloworld.c
@@ -0,0 +1,31 @@
+#include <linux/init.h>  //包含宏定义的头文件
+#include <linux/module.h>   //包含初始化加载模块的头文件
+
+//入口函数功能实现
+static int hello_init(void)
+{
+    //内核层只能使用printk，不能使用printf，因为内核层不支持C语言
+    printk(KERN_EMERG "[ KERN_EMERG ]  Hello  Module Init\n");
+    printk( "[ default ]  Hello  Module Init\n");
+    return 0;
+}
+
+//出口函数功能实现
+static void hello_exit(void)
+{
+    printk("[ default ]   Hello  World Exit\n");
+}
+
+module_init(hello_init); //驱动入口
+module_exit(hello_exit); //驱动出口
+
+MODULE_LICENSE("GPL v2"); //声明开源许可证
+// "GPL" 是指明 这是GNU General Public License的任意版本
+// “GPL v2” 是指明 这仅声明为GPL的第二版本
+// "GPL and addtional"
+// "Dual BSD/GPL"
+// "Dual MPL/GPL"
+// "Proprietary"  私有的
+MODULE_AUTHOR("embedfire"); //声明作者信息
+MODULE_DESCRIPTION("hello world"); //对这个模块作一个简单的描述
+MODULE_ALIAS("hello world_test");  //这个模块的别名
